254d5079507fe40359be677858cdb656ad3f25fd
jinjamb <jinjamb09@gmail.com>
2024-11-01 11:26:34 +0100

    DR2 revoir l'ajout d'indices

--- sudokusolver/DR2.java
+++ sudokusolver/DR2.java
@@ -10,7 +10,8 @@ public class DR2 extends DeductionRule{
         for (int i = 0; i < 81; i++) {
             if (sudoku.grid[i] == -1){
                 for (int numToPlace = 1; numToPlace <= 9; numToPlace++){
-                    if (!isPresent(sudoku, i, numToPlace)){
+                    int counter = isPresent(sudoku, i, numToPlace);
+                    if (counter == 1){
                         sudoku.set(numToPlace, i);
                         break;
                     }
@@ -20,16 +21,17 @@ public class DR2 extends DeductionRule{
         sudoku.afficher();
     }
 
-    public boolean isPresent(Grille sudoku, int index, int numToPlace){
+    public int isPresent(Grille sudoku, int index, int numToPlace){
         int rowStart = 9*(index / 9);
         int colStart = index % 9;
         int boxStart = 27 * (index / 27) + 3 * ((index % 9) / 3);
+        int counter = 0;
 
         for (int ind = 0; ind < 9; ind++){
             if (sudoku.grid[rowStart + ind] == numToPlace || sudoku.grid[9 * ind + colStart] == numToPlace || sudoku.grid[boxStart + ind % 3 + 9 * (ind / 3)] == numToPlace){
-                return true;
+                counter++;
             }
         }
-        return false;
+        return counter;
     }
 }
